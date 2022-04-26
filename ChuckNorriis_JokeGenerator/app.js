229486ac56d1e406133a87e510ce7010e64b21d4
Sidharth Gopalakrishnan <sidharthgopalakrishnan@gmail.com>
2022-04-26 09:15:31 +0530

    Have implemented the same project using async await. Need to add error handling

--- ChuckNorriis_JokeGenerator/app.js
+++ ChuckNorriis_JokeGenerator/app.js
@@ -1,30 +1,18 @@
+let results = document.getElementById("results");
+
 document.querySelector(".get-jokes").addEventListener("click", loadJokes);
 
-function loadJokes(e) {
-  const number = document.querySelector("#number").value;
-
-  const xhr = new XMLHttpRequest();
-
-  xhr.open("GET", `http://api.icndb.com/jokes/random/${number}`, true);
-  let output = "";
-  xhr.onload = function () {
-    if (this.status === 200) {
-      const response = JSON.parse(this.responseText);
-      console.log(response);
-
-      if (response.type === "success") {
-        const arr = response.value;
-        arr.forEach(function (element) {
-          output += `<li>${element.joke}</li>`;
-        });
-
-        document.querySelector(".results").innerHTML = output;
-      } else {
-        output += `<li>Something went wrong deer</li>`;
-      }
-    }
-  };
-
-  xhr.send();
+async function loadJokes(e) {
   e.preventDefault();
+  let out = "";
+  let number = document.querySelector("#input").value;
+  console.log(number);
+  const res = await fetch(`http://api.icndb.com/jokes/random/${number}`);
+  let data = await res.json();
+  console.log(results);
+  data.value.forEach((item) => {
+    out += `<li>${item.joke}</li>`;
+  });
+  results.innerHTML = out;
+  console.log(results);
 }
